c6c290196597ee676b8e7606ce27169d1f7c6254
SleepingTurtle <joshcharlestechie@gmail.com>
2018-03-27 17:46:45 -0400

    Added install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Deletes old files
+rm ~/.bashrc
+rm ~/.bash_aliases
+rm ~/.bash_exports
+rm ~/.bash_wrappers
+rm ~/.vimrc
+rm ~/.tmux.conf
+
+# Creates symlinks
+ln -s dotfiles/.bashrc ~/
+ln -s dotfiles/.bash_aliases ~/
+ln -s dotfiles/.bash_exports ~/
+ln -s dotfiles/.vimrc ~/
+ln -s dotfiles/.tmux.conf ~/
